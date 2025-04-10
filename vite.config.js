cc67f6478a2a5542996c1d884b54499b608c13ef
Sanerita <sanelisiwe.sileku@gmail.com>
2025-04-11 01:12:48 +0200

    PWA Support, Performance Optimization, Sitemap & Robots, Accessibility

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'sitemap.xml'],
+      manifest: {
+        name: 'ELPEAP Group',
+        short_name: 'ELPEAP',
+        description: 'Digital Solutions & Business Services',
+        theme_color: '#45a049',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,jpg}']
+      }
+    })
+  ],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          bootstrap: ['bootstrap', 'react-bootstrap', 'bootstrap-icons'],
+        }
+      }
+    }
+  }
 })
